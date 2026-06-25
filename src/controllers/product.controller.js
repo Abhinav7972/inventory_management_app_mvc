359d9871b689826424584916c60c57c79825e1b1
abhinav soni <abhinavsoni316@gmail.com>
2026-06-25 15:21:52 +0530

    added server side validation

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,12 +13,40 @@ export default class ProductController
 
  getProductForm(req,res)
  {
-   return res.render('new-products')
+   return res.render('new-products',{errorMessage:null})
  }
  
  addNewProduct(req,res)
  {
    console.log(req.body)
+   //validate data
+   const{name,price,image} = req.body;
+   let errors=[];
+   if(!name ||name.trim()=='')
+   {
+    errors.push('Name is required')
+   }
+   if(!price || parseFloat(price)<0)
+   {
+    errors.push('price must be positive')
+   }
+  
+   try
+   {
+     const validURL = new URL(image);
+   }
+   catch(err)
+   {
+   errors.push('invalid URL');
+   }
+
+   if(errors.length>0)
+   {
+    return res.render('new-products',{
+      errorMessage:errors[0],
+    })
+   }
+
     ProductModel.add(req.body)
     const products = ProductModel.get()
   res.render("products",{products}) 
