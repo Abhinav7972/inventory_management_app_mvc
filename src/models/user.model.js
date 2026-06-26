b492a740b2eccc77cf4ff610f73f5bba6ca79817
abhinav soni <abhinavsoni316@gmail.com>
2026-06-26 20:53:52 +0530

    added isValid method

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,6 +13,12 @@ export default class UserModel
         const newUser =  new UserModel(users.length + 1,name,email,password);
         users.push(newUser);
     }
+
+    static isvalidUser(email,password)
+    {
+        const result = users.find(u => u.email === email && u.password === password);
+        return result;
+    }
 }
 
 const users = []
